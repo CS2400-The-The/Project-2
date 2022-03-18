b54c5109b718fe4a5f9d296bf4580a3616b468f9
jamesjhansen <jamesjhansenh@gmail.com>
2022-03-18 15:59:31 -0700

    Add Junit dependencies and test file

---
+++ src/com/jamesjhansen/CalcTest.java
@@ -0,0 +1,17 @@
+package com.jamesjhansen;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalcTest {
+
+    @Test
+    void convertToPostfixTest() {
+
+    }
+
+    @Test
+    void evaluatePostfixTest() {
+
+    }
+}
